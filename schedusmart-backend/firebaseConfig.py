869591b9fd98cd0aa499b98f58d54d7aa7d7efc3
nicatght <0327stanley@gmail.com>
2024-02-17 16:30:48 -0500

    testing for gitignore

---
+++ schedusmart-backend/firebaseConfig.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+  'apiKey': "AIzaSyC53hV188tV2CxH8dWhgSSuIbIWuGDLl6A",
+  'authDomain': "schedusmart-483d7.firebaseapp.com",
+  'projectId': "schedusmart-483d7",
+  'storageBucket': "schedusmart-483d7.appspot.com",
+  'messagingSenderId': "442656439492",
+  'appId': "1:442656439492:web:fbff447025b995d1f89d91",
+  'measurementId': "G-LKY6EYQRS4",
+  'databaseURL': "https://schedusmart-483d7-default-rtdb.firebaseio.com/"}
