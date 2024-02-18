b246eb19f9bfe9f8356c8efaa02598f4a95ffa85
Himanshu <sinhah239@gmail.com>
2024-02-18 15:39:19 -0500

    readded some testing stuff

--- schedusmart-frontend/src/pages/Welcome.jsx
+++ schedusmart-frontend/src/pages/Welcome.jsx
@@ -6,12 +6,24 @@ export default function Welcome() {
     const handleButtonClick = () => {
         window.location.href = '/createaccount';
       };
+
+    const [goToDash, setGoToDash] = React.useState(false)
+
+    if (goToDash) {
+        return(
+            <>
+            <Navigate to="/dashboard" />
+            </>
+        );
+    }
+
     return(
         <>
         <Header/>
         <h2>Welcome!</h2>
         <button>Sign-in! This does nothing until sign-in is implemented</button>
         <button onClick={handleButtonClick}>Create an account!</button>
+        <button onClick={() => {setGoToDash(true)}}>Dashboard | For testing purposes</button>
         </>
     );
 }
