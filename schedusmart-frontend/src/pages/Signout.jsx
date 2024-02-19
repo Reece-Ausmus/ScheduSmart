a450a6a9642475d5206d7e102f9ec89d31f82990
cassie0206 <cassie610512@gmail.com>
2024-02-18 20:09:59 -0500

    having a pretty weird & ugly pop-up window, fixing it later

--- schedusmart-frontend/src/pages/Signout.jsx
+++ schedusmart-frontend/src/pages/Signout.jsx
@@ -1,12 +1,52 @@
 import Header from '../components/Header'
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap'
 import { Navigate } from 'react-router-dom'
 
-export default function Settings() {
-    return(
-        <>
-        <Header/>
-        <button>Sign out</button>
-        </>
-    );
+
+function SignoutWindow(props) {
+  const [goToWelcome, setGoToWelcome] = React.useState(false)
+
+  if (goToWelcome) {
+      return(
+          <>
+          <Navigate to="/welcome" />
+          </>
+      );
+  }
+
+  return (
+    <Modal
+      {...props}
+      size="sm"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered='true'
+      fullscreen='sm-down'
+    >
+      <Modal.Body>
+        <p>
+          Do you sure that you want to sign out?   
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="outline-primary" onClick={() => {setGoToWelcome(true)}} > Yes</Button>
+        <Button onClick={props.onHide}>No</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default function Logout() {
+  const [isClicked, setIsClicked] = useState(false);
+  return (
+    <>
+    <Header/>
+      <Button onClick={() => setIsClicked(true)} >Sign out</Button>
+      {isClicked &&
+        <SignoutWindow
+          show={isClicked}
+          onHide={() => setIsClicked(false)}
+        />}
+    </>
+  )
 }
