39aed3b773d88f45349e6c2d80ad78f66d9243e8
XUjiayi777 <jxugl777@gmail.com>
2024-02-18 23:14:12 -0500

    finish create-account page

--- schedusmart-frontend/src/pages/Createaccount.jsx
+++ schedusmart-frontend/src/pages/Createaccount.jsx
@@ -5,7 +5,8 @@ export default function Createaccount() {
     const [name, setName] = useState('');
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
-
+    const [password, setPassword] = useState('');
+    const [confirm_password, confirmpassword] = useState('');
     return (
         <div>
             <div className="headers" >
@@ -14,33 +15,30 @@ export default function Createaccount() {
             <div className="sub-headers">
                 <p> Complete your profile by filling in this account creation form </p>
             </div>
-            <div className="group-form name-form">
+            <div className="group name">
                 <label htmlFor="name">Name:</label>
-                <input
-                    type="text"
-                    id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
+                <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
             </div>
-            <div className="group-form username-form">
+            <div className="group username">
                 <label htmlFor="username">Username:</label>
-                <input
-                    type="text"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
+                <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}
                 />
             </div>
-            <div className="group-form email-form">
+            <div className="group email">
                 <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
+                <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
+            <div className="group password">
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div className="group confirm-password">
+                <label htmlFor="confirm-password">Confirm Password:</label>
+                <input type="password" id="confirm-password" value={confirm_password} onChange={(e) => confirmpassword(e.target.value)} />
+            </div>
+            <div className="create_button"><button type="submit">Create Account</button></div>
+
+
         </div>
     );
 }
