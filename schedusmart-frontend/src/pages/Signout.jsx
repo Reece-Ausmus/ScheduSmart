a518e55dd85815111717a652b2d27f216ee6412a
cassie0206 <cassie610512@gmail.com>
2024-02-18 22:14:49 -0500

    basically done only wait for login info

--- schedusmart-frontend/src/pages/Signout.jsx
+++ schedusmart-frontend/src/pages/Signout.jsx
@@ -4,7 +4,9 @@ import { Button, Modal } from 'react-bootstrap'
 import { Navigate } from 'react-router-dom'
 
 
-function SignoutWindow(props) {
+export default function Signout() {
+  const [isClicked, setIsClicked] = useState(false);
+
   const [goToWelcome, setGoToWelcome] = React.useState(false)
 
   if (goToWelcome) {
@@ -15,38 +17,20 @@ function SignoutWindow(props) {
       );
   }
 
-  return (
-    <Modal
-      {...props}
-      size="sm"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered='true'
-      fullscreen='sm-down'
-    >
-      <Modal.Body>
-        <p>
-          Do you sure that you want to sign out?   
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-primary" onClick={() => {setGoToWelcome(true)}} > Yes</Button>
-        <Button onClick={props.onHide}>No</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
+  const handleConfirmClick = () => {
+    if (window.confirm('Are you sure you want to sign out?')) {
+      //Yes
+      setGoToWelcome(true)
+    } else {
+      //No
+      // do nothing
+    }
+  };
 
-export default function Logout() {
-  const [isClicked, setIsClicked] = useState(false);
   return (
     <>
     <Header/>
-      <Button onClick={() => setIsClicked(true)} >Sign out</Button>
-      {isClicked &&
-        <SignoutWindow
-          show={isClicked}
-          onHide={() => setIsClicked(false)}
-        />}
+      <Button onClick={handleConfirmClick} >Sign out</Button>
     </>
   )
 }
