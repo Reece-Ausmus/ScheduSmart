fbf0cc0e2799e8feaa068de556ffe59d550d2cb5
sinhah239 <sinhah@purdue.edu>
2024-02-23 00:07:29 -0500

    Updated venv and added it to the gitignore

--- schedusmart-backend/.venv/Scripts/activate.bat
+++ schedusmart-backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\code\html\ScheduSmart\schedusmart-backend\.venv
+set VIRTUAL_ENV=C:\Users\Himanshu\Documents\School\Purdue\Junior\Spring Semester\Software\ScheduSmart\schedusmart-backend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
