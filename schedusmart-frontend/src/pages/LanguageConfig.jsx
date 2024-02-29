7a1cccd95679bad3f25c9ba86ca55ccf528c50a3
nicatght <0327stanley@gmail.com>
2024-02-29 17:20:13 -0500

    basic set up for language changer

---
+++ schedusmart-frontend/src/pages/LanguageConfig.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const LanguageSetting = React.createContext();
+
+export default LanguageSetting;
--- schedusmart-frontend/src/pages/Settings.jsx
+++ schedusmart-frontend/src/pages/Settings.jsx
@@ -1,24 +1,44 @@
-import Header from '../components/Header'
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-import AccountInfo from "./AccountInfo.jsx"
+import Header from "../components/Header";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import AccountInfo from "./AccountInfo.jsx";
+import LanguageSetting from "./LanguageConfig.jsx";
+import languageData from "../components/language.json";
 
 export default function Settings() {
-    const [goToCalendar, setGoToCalendar] = React.useState(false)
+  const [goToCalendar, setGoToCalendar] = React.useState(false);
+  const [language, setLanguage] = useState(0);
 
-    if (goToCalendar) {
-        return(
-            <>
-            <Navigate to="/calendar"/>
-            </>
-        );
-    }
-
-    return(
-        <>
-        <h1>Settings</h1>
-        <AccountInfo/>
-        <button onClick={() => {setGoToCalendar(true)}}>Calendar</button>
-        </>
+  if (goToCalendar) {
+    return (
+      <>
+        <Navigate to="/calendar" />
+      </>
     );
+  }
+
+  return (
+    <>
+      <h1>Settings</h1>
+      <AccountInfo />
+      <button
+        onClick={() => {
+          setLanguage(1);
+          console.log(languageData[1].welcome);
+        }}
+      >
+        language
+      </button>
+      <button
+        onClick={() => {
+          setGoToCalendar(true);
+        }}
+      >
+        Calendar
+      </button>
+      <LanguageSetting.Provider
+        value={{ language, setLanguage }}
+      ></LanguageSetting.Provider>
+    </>
+  );
 }
