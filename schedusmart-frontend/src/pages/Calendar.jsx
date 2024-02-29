e4030de9f4a5d30d1de50b047217df3b3a1bc1ba
cassie0206 <cassie610512@gmail.com>
2024-02-29 02:19:51 -0500

    day, week, year can change

--- schedusmart-frontend/src/pages/Calendar.jsx
+++ schedusmart-frontend/src/pages/Calendar.jsx
@@ -26,200 +26,22 @@ export default function MainFrame() {
           className="calender2"
           style={{ display: selectMode === 2 ? "block" : "none" }}
         >
-          <p className="weekday" id="weekday1">
-            Su.
-          </p>
-          <p className="weekday" id="weekday2">
-            Mo.
-          </p>
-          <p className="weekday" id="weekday3">
-            Tu.
-          </p>
-          <p className="weekday" id="weekday4">
-            We.
-          </p>
-          <p className="weekday" id="weekday5">
-            Th.
-          </p>
-          <p className="weekday" id="weekday6">
-            Fr.
-          </p>
-          <p className="weekday" id="weekday7">
-            Sa.
-          </p>
-          <div className="dayBox1">
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-          </div>
-          <div className="dayBox2">
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-          </div>
-          <div className="dayBox3">
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-          </div>
-          <div className="dayBox4">
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-          </div>
-          <div className="dayBox5">
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-          </div>
-          <div className="dayBox6">
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-          </div>
-          <div className="dayBox7">
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-          </div>
-          <div className="daytimebox">
-            <p className="dayWord">1:00</p>
-            <p className="dayWord">3:00</p>
-            <p className="dayWord">5:00</p>
-            <p className="dayWord">7:00</p>
-            <p className="dayWord">9:00</p>
-            <p className="dayWord">11:00</p>
-            <p className="dayWord">13:00</p>
-            <p className="dayWord">15:00</p>
-            <p className="dayWord">17:00</p>
-            <p className="dayWord">19:00</p>
-            <p className="dayWord">21:00</p>
-            <p className="dayWord">23:00</p>
-          </div>
+          {WeekStyle()}
         </div>
         <div
           className="calender1"
           style={{ display: selectMode === 1 ? "block" : "none" }}
         >
-          <div className="dayBox">
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-            <hr className="dayLine" />
-          </div>
-          <div className="daytimebox">
-            <p className="dayWord">1:00</p>
-            <p className="dayWord">3:00</p>
-            <p className="dayWord">5:00</p>
-            <p className="dayWord">7:00</p>
-            <p className="dayWord">9:00</p>
-            <p className="dayWord">11:00</p>
-            <p className="dayWord">13:00</p>
-            <p className="dayWord">15:00</p>
-            <p className="dayWord">17:00</p>
-            <p className="dayWord">19:00</p>
-            <p className="dayWord">21:00</p>
-            <p className="dayWord">23:00</p>
-          </div>
+          {DayStyle()}
         </div>
         <div style={{ display: selectMode === 3 ? "block" : "none" }}>
           <div>
-            <div>{MonthStyle()}</div>
+            {MonthStyle()}
           </div>
         </div>
 
         <div style={{ display: selectMode === 4 ? "block" : "none" }}>
-          <table>
-            <tr class="MonthBoxFor4">
-              <td class="MonthBoxFor4">January</td>
-              <td class="MonthBoxFor4">Febuary</td>
-              <td class="MonthBoxFor4">March</td>
-              <td class="MonthBoxFor4">April</td>
-            </tr>
-            <tr class="MonthBoxFor4">
-              <td class="MonthBoxFor4">May</td>
-              <td class="MonthBoxFor4">June</td>
-              <td class="MonthBoxFor4">July</td>
-              <td class="MonthBoxFor4">August</td>
-            </tr>
-            <tr class="MonthBoxFor4">
-              <td class="MonthBoxFor4">September</td>
-              <td class="MonthBoxFor4">October</td>
-              <td class="MonthBoxFor4">November</td>
-              <td class="MonthBoxFor4">December</td>
-            </tr>
-          </table>
+            {YearStyle()}
         </div>
       </div>
     );
@@ -344,7 +166,8 @@ export default function MainFrame() {
             document.getElementById("2").style.backgroundColor = "#2d2d2d";
             document.getElementById("3").style.backgroundColor = "#2d2d2d";
             document.getElementById("4").style.backgroundColor = "#cfcfcf";
-            setDetailInfo(todayYear);
+            // here is where origin year display
+            //setDetailInfo(todayYear);
           }}
         >
           year
@@ -375,9 +198,9 @@ export default function MainFrame() {
             document.getElementById("2").style.backgroundColor = "#cfcfcf";
             document.getElementById("3").style.backgroundColor = "#2d2d2d";
             document.getElementById("4").style.backgroundColor = "#2d2d2d";
-            setDetailInfo(
-              String(temp)
-            );
+            //setDetailInfo(
+            //</div>  String(temp)
+            //);
           }}
         >
           week
@@ -392,9 +215,10 @@ export default function MainFrame() {
             document.getElementById("2").style.backgroundColor = "#2d2d2d";
             document.getElementById("3").style.backgroundColor = "#2d2d2d";
             document.getElementById("4").style.backgroundColor = "#2d2d2d";
-            setDetailInfo(
-              String(temp)
-            );
+            // here is where origin day display
+            //setDetailInfo(
+            //</div>  String(temp)
+            //);
           }}
         >
           day
@@ -402,7 +226,7 @@ export default function MainFrame() {
       </div>
     );
   }
-  const temp = moment().format('MM/DD');
+  const temp = moment().format('YY/MM/DD');
   const today = new Date();
   const lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
   const todayMonth = today.getMonth();
@@ -426,9 +250,9 @@ export default function MainFrame() {
   const lastDayInt = Math.floor(lastDay.getDate());
   let date = todayseeker();
 
-  const [detailInfo, setDetailInfo] = useState(
-    String(today.getMonth()) + "/" + String(today.getDate())
-  );
+  //const [detailInfo, setDetailInfo] = useState(
+  //  String(today.getMonth()) + "/" + String(today.getDate())
+  //);
 
   const [selectMode, setSelectMode] = useState(1);
 
@@ -453,11 +277,216 @@ export default function MainFrame() {
     }
   };
 
-const [currentMonth, setCurrentMonth] = useState(d);
 
-  // handle month 
+// handle day
+function DayStyle(){
+  const d = moment();
+
+  const [currentDay, setCurrentDay] = useState(d);
+
+  const incrementMonth = () => {
+    setCurrentDay(moment(currentDay.add(1, "days")));
+  }
+
+  const decrementMonth = () => {
+    setCurrentDay(moment(currentDay.subtract(1, "days")));
+  }
+
+  return(
+    <div>
+      <button>{currentDay.format('MM/DD')}</button>
+      <button onClick={decrementMonth}>Prev</button><button onClick={incrementMonth}>Next</button>
+      <div className="dayBox">
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+        <hr className="dayLine" />
+      </div>
+      <div className="daytimebox">
+        <p className="dayWord">1:00</p>
+        <p className="dayWord">3:00</p>
+        <p className="dayWord">5:00</p>
+        <p className="dayWord">7:00</p>
+        <p className="dayWord">9:00</p>
+        <p className="dayWord">11:00</p>
+        <p className="dayWord">13:00</p>
+        <p className="dayWord">15:00</p>
+        <p className="dayWord">17:00</p>
+        <p className="dayWord">19:00</p>
+        <p className="dayWord">21:00</p>
+        <p className="dayWord">23:00</p>
+      </div>
+    </div>
+  );
+};
+
+// handle week
+function WeekStyle(){
+  const d = moment();
+
+  const [currentWeek, setCurrentWeek] = useState(d);
+
+  const incrementMonth = () => {
+    setCurrentWeek(moment(currentWeek.add(1, "weeks")));
+  }
+
+  const decrementMonth = () => {
+    setCurrentWeek(moment(currentWeek.subtract(1, "weeks")));
+  }
+
+  return(
+  <div>
+    <button>{currentWeek.format('MM/DD')}</button>
+    <button onClick={decrementMonth}>Prev</button><button onClick={incrementMonth}>Next</button>
+    <p className="weekday" id="weekday1">
+      Su.
+    </p>
+    <p className="weekday" id="weekday2">
+      Mo.
+    </p>
+    <p className="weekday" id="weekday3">
+      Tu.
+    </p>
+    <p className="weekday" id="weekday4">
+      We.
+    </p>
+    <p className="weekday" id="weekday5">
+      Th.
+    </p>
+    <p className="weekday" id="weekday6">
+      Fr.
+    </p>
+    <p className="weekday" id="weekday7">
+      Sa.
+    </p>
+    <div className="dayBox1">
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+    </div>
+    <div className="dayBox2">
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+    </div>
+    <div className="dayBox3">
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+    </div>
+    <div className="dayBox4">
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+    </div>
+    <div className="dayBox5">
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+    </div>
+    <div className="dayBox6">
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+    </div>
+    <div className="dayBox7">
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+      <hr className="dayLine" />
+    </div>
+    <div className="daytimebox">
+      <p className="dayWord">1:00</p>
+      <p className="dayWord">3:00</p>
+      <p className="dayWord">5:00</p>
+      <p className="dayWord">7:00</p>
+      <p className="dayWord">9:00</p>
+      <p className="dayWord">11:00</p>
+      <p className="dayWord">13:00</p>
+      <p className="dayWord">15:00</p>
+      <p className="dayWord">17:00</p>
+      <p className="dayWord">19:00</p>
+      <p className="dayWord">21:00</p>
+      <p className="dayWord">23:00</p>
+    </div>
+  </div>);
+};
+
+// handle month 
 function MonthStyle() {  
-    //const [currentMonth, setCurrentMonth] = useState(d);
+    const d = moment();
+
+    const [currentMonth, setCurrentMonth] = useState(d);
 
     const incrementMonth = () => {
       setCurrentMonth(moment(currentMonth.add(1, "months")));
@@ -467,10 +496,9 @@ function MonthStyle() {
       setCurrentMonth(moment(currentMonth.subtract(1, "months")));
     }
 
-
     return (
       <div >
-        <button>{currentMonth.format('MMMM')}</button>
+        <button>{currentMonth.format('MMMM YYYY')}</button>
         <button onClick={decrementMonth}>Prev</button><button onClick={incrementMonth}>Next</button>
         <div>
             <table>
@@ -548,13 +576,55 @@ function MonthStyle() {
         }
   };
 
+  // handle year style
+  function YearStyle() {
+    const d = moment();
+
+    const [currentYear, setCurrentYear] = useState(d);
+
+    const incrementMonth = () => {
+      setCurrentYear(moment(currentMonth.add(1, "years")));
+    }
+
+    const decrementMonth = () => {
+      setCurrentYear(moment(currentMonth.subtract(1, "years")));
+    }
+
+    return(
+      <div >
+        <button>{currentYear.format('YYYY')}</button>
+        <button onClick={decrementMonth}>Prev</button><button onClick={incrementMonth}>Next</button>
+          <table>
+            <tr class="MonthBoxFor4">
+              <td class="MonthBoxFor4">January</td>
+              <td class="MonthBoxFor4">Febuary</td>
+              <td class="MonthBoxFor4">March</td>
+              <td class="MonthBoxFor4">April</td>
+            </tr>
+            <tr class="MonthBoxFor4">
+              <td class="MonthBoxFor4">May</td>
+              <td class="MonthBoxFor4">June</td>
+              <td class="MonthBoxFor4">July</td>
+              <td class="MonthBoxFor4">August</td>
+            </tr>
+            <tr class="MonthBoxFor4">
+              <td class="MonthBoxFor4">September</td>
+              <td class="MonthBoxFor4">October</td>
+              <td class="MonthBoxFor4">November</td>
+              <td class="MonthBoxFor4">December</td>
+            </tr>
+          </table>
+        </div>
+    );
+  };
+
 
   return (
     <div className="container">
 
       <div className="calender_container">
         <div className="calender_container_controlbar">
-          <h2 className="detailInfo">{detailInfo}</h2>
+          {/*<h2 className="detailInfo">{detailInfo}</h2> */}
           <div>{calendarControlFlowButtonPackage()}</div>
         </div>
         <div>{fourCalendarPackage()}</div>
