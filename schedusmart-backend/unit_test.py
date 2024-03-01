06e9e3b8365e79cd8412e0e543a29b1964912a12
Bradnorris1 <norri103@purdue.edu>
2024-03-01 04:12:02 -0500

    add update_acc test, remove node_modules

--- schedusmart-backend/unit_test.py
+++ schedusmart-backend/unit_test.py
@@ -1,4 +1,5 @@
 from fire_base import *
+from route import *
 
 user = {
     "email": "unitTest@purpose.only",
@@ -42,9 +43,37 @@ def creating_same_email():
 def testToPullAccountInfo():
     pass
 
+def test_update_account_info():
+    # Assuming the user has been created successfully in the previous tests
+    temp_user = {"email": "unitTest@purpose.only", "password": "ThisIsJustPassword"}
+    user_id = login_account_with_email_and_password(temp_user)
 
-print("\n------unit test for account-----------")
+    # Update account information
+    updated_info = {
+        "user_id": user_id,
+        "firstname": "UpdatedFirstName",
+        "lastname": "UpdatedLastName",
+        "username": "UpdatedUsername"
+    }
+
+    try:
+        ret = update_user_info(updated_info)
+        assert ret == 0 
+        print("update_account_info\npass\n")
+    except:
+        traceback.print_exc()
+        print("update_account_info\nfail\n")
+        assert False
+
+    # Clean up - delete the user
+    delete_account(user_id)
+
+
+
+print("\n-----------unit test for account-----------")
 testing_creating_invalid_account()
 print("testing_creating_invalid_account\npass\n")
 creating_same_email()
 print("creating_same_email\npass\n")
+test_update_account_info()
+print("test_update_account_info\npass\n")
