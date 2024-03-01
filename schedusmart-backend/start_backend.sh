f737cd56c183862ab2158cc9dcc3acab663482f1
Reece-Ausmus <112425322+Reece-Ausmus@users.noreply.github.com>
2024-02-29 21:11:44 -0500

    updated scripts

--- schedusmart-backend/start_backend.sh
+++ schedusmart-backend/start_backend.sh
@@ -2,7 +2,7 @@
 
 #install dependencies
 echo "Installing dependencies..."
-pip install -r requirements.txt > /dev/null
+pip install -r requirements.txt > /dev/null 2> /dev/null
 
 #start flask
 flask --app server.py run
--- schedusmart-frontend/start_frontend.sh
+++ schedusmart-frontend/start_frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #remove outdated info
-rm -rf node_modules/.vite &
+rm -rf node_modules/.vite >/dev/null 2>/dev/null
 
 npm run dev
