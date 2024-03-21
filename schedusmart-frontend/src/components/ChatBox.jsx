ed768055072ecb02195e6924c7654243337acc38
nicatght <0327stanley@gmail.com>
2024-03-21 12:49:10 -0400

    Create UI of chat box

---
+++ schedusmart-frontend/src/components/ChatBox.jsx
@@ -0,0 +1,26 @@
+export default function chatBox() {
+    return (<>
+        <div class="chat_box_container">
+          <div class="info">Friends</div>
+    
+          <div class="messageBox">
+            <p class="rec">Hi</p>
+            <p class="sent">Hi</p>
+            <p class="rec">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto culpa sequi praesentium, eos animi iusto, fuga eaque magni repellat eum, assumenda odit! Excepturi dolore veritatis aspernatur vitae sint. Quos, nesciunt?</p>
+            <p class="sent">Hi?</p>
+            <p class="sent">Hi?</p>
+            <p class="sent">Hi?</p>
+            <p class="sent">Hi?</p>
+            <p class="sent">Hi?</p>
+            <p class="sent">Hi?</p>
+            <p class="sent">Hi?</p>
+            <p class="sent">Hi?</p>
+          </div>
+    
+          <div class="footer">
+            <input class="footer_input" type="text" name=""/>
+            <button class="footer_button">SEND</button>
+          </div>
+        </div>
+    </>);
+}
