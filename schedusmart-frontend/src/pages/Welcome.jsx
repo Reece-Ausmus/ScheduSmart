a463502d652fc954eff956fbfcedad2e786468d3
XUjiayi777 <jxugl777@gmail.com>
2024-03-25 23:42:59 -0400

    styling welcome page

--- schedusmart-frontend/src/pages/Welcome.jsx
+++ schedusmart-frontend/src/pages/Welcome.jsx
@@ -1,66 +1,89 @@
 import React from "react";
 import Header from "../components/Header";
-import { Navigate } from "react-router-dom";
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { orange } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: orange,
+    secondary: {
+      main: "#ab5600",
+    },
+  },
+});
 
 export default function Welcome() {
-  /*const [goToDash, setGoToDash] = React.useState(false)
-
-    if (goToDash) {
-        return(
-            <>
-            <Navigate to="/dashboard" />
-            </>
-        );
-    }*/
 
   return (
-    <>
-      <Header />
-      <h2>Welcome!</h2>
-      <button
-        onClick={() => {
-          window.location.href = "/signin";
+    <ThemeProvider theme={theme}>
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          pt: { xs: 14, sm: 20 },
+          pb: { xs: 8, sm: 12 },
         }}
       >
-        Sign-in!
-      </button>
-      <button
-        onClick={() => {
-          window.location.href = "/createaccount";
-        }}
-      >
-        Create an account!
-      </button>
-      {/* 
-      <button
-        onClick={() => {
-          window.location.href = "/dashboard";
-        }}
-      >
-        Dashboard
-      </button>
-      <button
-        onClick={() => {
-          window.location.href = "/taskmanager";
-        }}
-      >
-        Task Manager
-      </button>
-      <button
-        onClick={() => {
-          window.location.href = "/calendar";
-        }}
-      >
-        Calendar
-      </button>
-      <button
-        onClick={() => {
-          window.location.href = "/settings";
-        }}
-      >
-        Settings
-      </button>
-      */}
-    </>
+        <Stack spacing={2} useFlexGap sx={{ width: { xs: '100%', sm: '70%' } }}>
+          <Typography
+            variant="h1"
+            sx={{
+              display: 'flex',
+              flexDirection: { xs: 'column', md: 'row' },
+              alignSelf: 'center',
+              textAlign: 'center',
+              fontSize: 'clamp(3.5rem, 10vw, 4rem)',
+            }}
+          >
+            Welcome to&nbsp;
+            <Typography
+              component="span"
+              variant="h1"
+              sx={{
+                fontSize: 'clamp(3rem, 10vw, 4rem)',
+                color: (theme) =>
+                  theme.palette.mode === 'light' ? 'primary.main' : 'primary.light',
+              }}
+            >
+              Schedusmart!
+            </Typography>
+          </Typography>
+          <Typography
+            textAlign="center"
+            color="text.secondary"
+            sx={{ alignSelf: 'center', width: { sm: '100%', md: '80%' } }}
+          >
+            Schedusmart is a cutting-edge platform designed to streamline your life.
+            Our innovative tool combines assignment tracking, a fully functional calendar,
+            and an AI-powered scheduler to help you manage your tasks efficiently and effectively.
+            With Schedusmart, you can say goodbye to the complexities of scheduling and hello to a more organized, productive life
+          </Typography>
+          <Stack
+            direction={{ xs: 'column', sm: 'row' }}
+            alignSelf="center"
+            spacing={1}
+            useFlexGap
+            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
+          >
+            <Button variant="contained" onClick={() => { window.location.href = "/createaccount"; }}>Get started</Button>
+            <Button variant="contained" onClick={() => { window.location.href = "/signin"; }}>
+              Already have an account?
+            </Button>
+          </Stack>
+          <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+            By clicking &quot;Start now&quot; you agree to our&nbsp;
+            <Link href="#" color="primary">
+              Terms & Conditions
+            </Link>
+          </Typography>
+        </Stack>
+      </Container>
+    </ThemeProvider >
   );
 }
