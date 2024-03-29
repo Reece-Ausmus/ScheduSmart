68f73e06a387d414269aae15accf79440afdbd6a
sinhah239 <sinhah@purdue.edu>
2024-03-29 00:06:41 -0400

    Starting working on unit tests

---
+++ schedusmart-frontend/src/pages/__tests__/TaskManager.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import TaskManager from '../TaskManager'
+
+/**
+ * @jest-environment jsdom
+ */
+
+test(('testing the tester'), () => {
+    expect(true).toBe(true)
+})
+
+test(('Test 3 - Verify Progress Meter'), () => {
+    render(<TaskManager/>)
+})
