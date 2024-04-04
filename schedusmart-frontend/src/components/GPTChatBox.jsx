7de7d7dffbc75a7777eb30e13117cb74589943b8
sinhah239 <sinhah@purdue.edu>
2024-04-03 20:41:47 -0400

    begun work on chatbox

--- schedusmart-frontend/src/components/GPTChatBox.jsx
+++ schedusmart-frontend/src/components/GPTChatBox.jsx
@@ -6,6 +6,8 @@ import seachIcon from "./search-interface-symbol.png";
 export default function GPTChatBox() {
   
   const [isExpand, setIsExpand] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messageHistory, setMessageHistory] = useState(["new message", "hello new message", "new message"]);
 
   return (
     <>
@@ -16,13 +18,29 @@ export default function GPTChatBox() {
           console.log(isExpand);
         }}
       >
-        {isExpand ? "Collapse" : "Expand"}
+        {isExpand ? "Close ChatBot." : "Open ChatBot!"}
       </button>
       <div
         className="chat_box_container"
         style={{ display: isExpand ? "flex" : "none" }}
       >
-        This is the chat box
+        <div>
+          {messageHistory.map((chat) => {
+            chat[0]
+          })}
+        </div>
+        <div>
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => {setMessage(e.target.value)}}
+          />
+          <button onClick={() => {
+            setMessageHistory([...messageHistory, message]);
+            setMessage("")
+            console.log(messageHistory)
+          }}>{"=>"}</button>
+        </div>
       </div>
     </>
   );
