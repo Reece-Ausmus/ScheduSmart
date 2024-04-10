504c0a23c8858fb1856757de8b413f39b5add7bb
nicatght <0327stanley@gmail.com>
2024-04-10 12:11:57 -0400

    add send message function

--- schedusmart-backend/friendManage.py
+++ schedusmart-backend/friendManage.py
@@ -2,8 +2,9 @@ from fire_base import *
 
 
 data = {
-    "user_id": "fdBqeU406KdQ5YKMjoManKCxYiF2",
-    "name": "stanley",
-    "confirm": False
+    "user_id": "3eB0n2XFmDgeI8LXjwotNqNvl5l1",
+    "name": "gloria_xu",
+    "confirm": False,
+    "message": "hello?"
 }
 
