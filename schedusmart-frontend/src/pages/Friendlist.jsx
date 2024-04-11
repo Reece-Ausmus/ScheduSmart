d16537edd58067c76e58d56bb4ed9d71ea17f564
XUjiayi777 <jxugl777@gmail.com>
2024-04-11 15:46:37 -0400

    update request list

--- schedusmart-frontend/src/pages/Friendlist.jsx
+++ schedusmart-frontend/src/pages/Friendlist.jsx
@@ -5,13 +5,15 @@ import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import CssBaseline from '@mui/material/CssBaseline';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
 import Dashboard from "./Dashboard";
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import Fab from "@mui/material/Fab";
 import ArchiveIcon from '@mui/icons-material/Archive';
 import Paper from '@mui/material/Paper';
-import List from '@mui/material/List';
+import { List, ListItemSecondaryAction } from '@mui/material';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -19,7 +21,7 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import Avatar from '@mui/material/Avatar';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { orange, grey } from "@mui/material/colors";
+import { orange } from "@mui/material/colors";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -30,6 +32,8 @@ import DialogTitle from '@mui/material/DialogTitle';
 import send_request from "./requester";
 import { Link as RouterLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import MessageIcon from '@mui/icons-material/Message';
+import IconButton from '@mui/material/IconButton';
 
 // user_id to get user info
 const userId = sessionStorage.getItem("user_id");
@@ -43,35 +47,36 @@ const theme = createTheme({
     },
 });
 
+// Get messages
+const messageExamples = [
+    {
+        name: 'Stanley',
+        message1: "I'll be in the neighbourhood this week. Let's grab a bite to eat",
+        person: '/static/images/avatar/5.jpg',
+        id: 1,
+    },
+    {
+        name: 'Cassie',
+        message1: "I'll be in the neighbourhood this week. Let's grab a bite to eat",
+        person: '/static/images/avatar/5.jpg',
+        id: 2,
+    },
+];
 function refreshMessages() {
     return messageExamples;
 }
-const messageExamples = [
-    {
-        primary: 'Brunch this week?',
-        secondary: "I'll be in the neighbourhood this week. Let's grab a bite to eat",
-        person: '/static/images/avatar/5.jpg',
-    },
-];
-const friends = [
+
+// const messages = async()=> {
+//     const response = await send_request("/get_messages", { "user_id": userId, "name":friend,"start_point":0});
+
+// }
+
+// Get friend list
+const nameList = [
     { id: 1, name: 'Friend 1' },
     { id: 2, name: 'Friend 2' },
     { id: 3, name: 'Friend 3' },
-  ];
-
-// function getNameList(){
-//     const [friends, setFriends] = useState();
-//     const NameList = async () => {
-//         const response = await send_request("/get_friends", { "user_id": userId });
-//         const request_list=response.request;
-//         console.log(request_list.chatroom);
-//         const updatedFriends = request_list.map((friend, index) => ({
-//             id: friend.id,
-//             name: friend.name
-//         }));
-//         setFriends(updatedFriends);
-//     };
-// }
+];
 
 
 const Link = RouterLink;
@@ -93,10 +98,11 @@ ListItemLink.propTypes = {
 };
 
 export default function Friendlist() {
+    // invitation
     const [value, setValue] = useState(0);
     const ref = useRef(null);
     const [messages, setMessages] = useState(() => refreshMessages());
-    const [open, setOpen] = useState(false);
+    const [invitationopen, setInvitationOpen] = useState(false);
     const [Props, setProps] = useState({ options: [] });
     const handleSearchUser = async (event, name) => {
         if (!name) {
@@ -108,14 +114,13 @@ export default function Friendlist() {
         setProps({ options: name_list });
     };
 
-    const handleClickOpen = () => {
-        setOpen(true);
+    const handleInvitationClickOpen = () => {
+        setInvitationOpen(true);
     };
-    const handleClose = () => {
-        setOpen(false);
+    const handleInvitationClose = () => {
+        setInvitationOpen(false);
     };
-
-    const handleSubmit = async () => {
+    const handleInvitationSubmit = async () => {
         console.log("submit");
         const username = document.getElementById("username").value;
         const regex = /[\\"\s\'\\\x00-\x1F\x7F]/g;
@@ -144,6 +149,26 @@ export default function Friendlist() {
         }
     };
 
+    // Request list
+    const [requestopen, SetRequestOpen] = useState(false);
+    const [requestList, setRequestList] = useState(null);
+    const getRequestList = async () => {
+        const response = await send_request("/get_friends", { "user_id": userId });
+        const request_list = response.request;
+        setRequestList(request_list);
+    };
+    const handleRequestClickOpen = () => {
+        SetRequestOpen(true);
+    };
+    const handleRequestClose = () => {
+        SetRequestOpen(false);
+    };
+    useEffect(() => {
+        if (requestopen) {
+            getRequestList();
+        }
+    }, [requestopen]);
+
     useEffect(() => {
         ref.current.ownerDocument.body.scrollTop = 0;
         setMessages(refreshMessages());
@@ -158,11 +183,11 @@ export default function Friendlist() {
                     aria-label="add"
                     color="primary"
                     size="small">
-                    <AddIcon onClick={handleClickOpen} />
+                    <AddIcon onClick={handleInvitationClickOpen} />
                 </Fab>
                 <Dialog
-                    open={open}
-                    onClose={handleClose}
+                    open={invitationopen}
+                    onClose={handleInvitationClose}
                     PaperProps={{
                         component: 'form',
                     }}>
@@ -187,36 +212,67 @@ export default function Friendlist() {
                             )} />
                     </DialogContent>
                     <DialogActions>
-                        <Button onClick={handleSubmit}>Send invitation</Button>
-                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={handleInvitationSubmit}>Send invitation</Button>
+                        <Button onClick={handleInvitationClose}>Cancel</Button>
+                    </DialogActions>
+                </Dialog>
+                <Fab
+                    aria-label="friend request"
+                    color="primary"
+                    size="small">
+                    <MessageIcon onClick={handleRequestClickOpen} />
+                </Fab>
+                <Dialog
+                    open={requestopen}
+                    onClose={handleRequestClose}
+                    PaperProps={{
+                        component: 'form',
+                        sx: {
+                            width: 'auto',
+                            maxWidth: 'sm',
+                        }
+                    }}>
+                    <DialogTitle>Friend request</DialogTitle>
+                    <DialogContent>
+                        <List sx={{ width: 500 }}>
+                            {requestList && requestList.map(({ name, confirm, chatroom }, index) => (
+                                <ListItem key={name+index}>
+                                    <ListItemText primary={`${name} wants to add you as a friend`} />
+                                    <ListItemSecondaryAction>
+                                        <IconButton edge="end" aria-label="accept" sx={{ color: 'green' }}>
+                                            <CheckIcon />
+                                        </IconButton>
+                                        <IconButton edge="end" aria-label="reject" sx={{ color: 'red', marginLeft: 3 }}>
+                                            <CloseIcon />
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleRequestClose}>Back</Button>
                     </DialogActions>
                 </Dialog>
             </div>
             <Box sx={{ pb: 7 }} ref={ref}>
                 <CssBaseline />
                 <List>
-                    {friends.map((friend) => (
-                        <ListItemLink key={friend.id} to={`/chat/${friend.id}`} primary={friend.name} />
-                    ))}
-                </List>
-                {/* <List>
-                    {messages.map(({ primary, secondary, person }, index) => (
-                        <ListItemButton key={index + person}>
+                    {messages.map(({ name, message1, person, id }, index) => (
+                        <ListItemButton key={index + person} component={Link} to={`/chat/${id}`}>
                             <ListItemAvatar>
                                 <Avatar alt="Profile Picture" src={person} />
                             </ListItemAvatar>
-                            <ListItemText primary={primary} secondary={secondary} />
-                        </ListItemButton>
-                    ))}
-                </List> */}
+                            <ListItemText primary={name} secondary={message1} />
+                        </ListItemButton>))}
+                </List>
                 <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
                     <BottomNavigation
                         showLabels
                         value={value}
                         onChange={(event, newValue) => {
                             setValue(newValue);
-                        }}
-                    >
+                        }}>
                         <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
                         <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
                         <BottomNavigationAction label="Archive" icon={<ArchiveIcon />} />
