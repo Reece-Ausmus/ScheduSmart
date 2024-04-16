7e2ad4a720eda26d6eabf10fad624c24b6fec2b8
Reece-Ausmus <112425322+Reece-Ausmus@users.noreply.github.com>
2024-04-15 21:33:56 -0400

    basic outline for datapage

--- schedusmart-frontend/src/pages/DataPage.jsx
+++ schedusmart-frontend/src/pages/DataPage.jsx
@@ -1,36 +1,102 @@
-import { useState } from "react";
-import Avatar from "@mui/material/Avatar";
+import React, { useState, useEffect } from "react";
+
+import { ResponsiveChartContainer, BarPlot } from "@mui/x-charts";
+import { saveAs } from "file-saver";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { AppBar, IconButton, Menu, Toolbar } from "@mui/material";
 import { orange } from "@mui/material/colors";
-import send_request from "./requester";
-import { flaskURL, user_id } from "../config";
+import PropTypes from "prop-types";
+import { DataGrid } from "@mui/x-data-grid";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { BarChart } from "@mui/x-charts/BarChart";
+import Dashboard from "./Dashboard";
 
 const theme = createTheme({
   palette: {
-    primary: orange,
+    primary: {
+      main: orange[500],
+    },
     secondary: {
       main: "#ab5600",
     },
   },
+  components: {
+    MuiBarChart: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "gray",
+        },
+      },
+    },
+  },
 });
 
-export default function DataPage() {
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = [
+  "Page A",
+  "Page B",
+  "Page C",
+  "Page D",
+  "Page E",
+  "Page F",
+  "Page G",
+];
+
+export default function SimpleBarChart() {
+  const [timeFilter, setTimeFilter] = useState(0);
+  const handleTimeFilterChange = (e) => {
+    setTimeFilter(e.target.value);
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <h1>Data</h1>
+      <div>{Dashboard()}</div>
+      <Box sx={{ minWidth: 120, maxWidth: 300 }}>
+        <Typography
+          component="h1"
+          variant="h5"
+          style={{ marginBottom: "20px", marginTop: "20px" }}
+        >
+          Data Dashboard
+        </Typography>
+        <FormControl fullWidth margin="normal">
+          <InputLabel id="timeFilterLabel">Time Period</InputLabel>
+          <Select
+            labelId="timeFilterLabel"
+            id="timeFilterSelect"
+            value={timeFilter}
+            label="Time Filter"
+            onChange={handleTimeFilterChange}
+          >
+            <MenuItem value={0}>All</MenuItem>
+            <MenuItem value={7}>Past 7 Days</MenuItem>
+            <MenuItem value={30}>Past 30 Days</MenuItem>
+            <MenuItem value={365}>Past Year</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <BarChart
+        width={500}
+        height={300}
+        series={[
+          { data: pData, label: "pv", id: "pvId" },
+          { data: uData, label: "uv", id: "uvId" },
+        ]}
+        xAxis={[{ data: xLabels, scaleType: "band" }]}
+      />
     </ThemeProvider>
   );
 }
